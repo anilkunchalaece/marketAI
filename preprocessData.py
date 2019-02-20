7e6a78d4dd8c796f44b7fd528383a6987471409e
anilkunchalaece <anilkunchalece@gmail.com>
2019-02-20 07:57:55 +0530

    MNIST Format for images

---
+++ preprocessData.py
@@ -0,0 +1,61 @@
+import os
+import cv2
+import numpy as np
+
+class PreprocessData:
+    
+    def __init__(self, rootDir):
+        self.rootDir = rootDir
+        self.trainDir = os.path.join(rootDir,'Train')
+        self.validDir = os.path.join(rootDir,'Valid')
+        self.testDir = os.path.join(rootDir,'Test')
+
+        self.testData  = tuple()
+    
+    def readImageReturnArray(self,imageName):
+        imArray = cv2.imread(imageName,0)
+        # print("image shape is {}".format(imArray.shape))
+        #reshape the array
+        imArray = np.reshape(imArray,imArray.shape[0]*imArray.shape[1])
+        # print("image reshaped to {}".format(imArray.shape))
+        return imArray
+
+    def loadTestData(self) :
+        testFeatureArray =[]
+        testClassArray = []
+        for classDir in os.listdir(self.testDir) :
+            for imgName in os.listdir(os.path.join(self.testDir,classDir)) :
+                imgPath = os.path.join(self.testDir,classDir,imgName)
+                testFeatureArray.append(self.readImageReturnArray(imgPath))
+                testClassArray.append(classDir)
+        
+        return (np.array(testFeatureArray) , np.array(testClassArray))
+
+    def loadTrainData(self) :
+        trainFeatureArray =[]
+        trainClassArray = []
+        for classDir in os.listdir(self.trainDir) :
+            for imgName in os.listdir(os.path.join(self.trainDir,classDir)) :
+                imgPath = os.path.join(self.trainDir,classDir,imgName)
+                trainFeatureArray.append(self.readImageReturnArray(imgPath))
+                trainClassArray.append(classDir)
+        
+        return (np.array(trainFeatureArray) , np.array(trainClassArray))
+
+    def loadValidData(self) :
+        validFeatureArray =[]
+        validClassArray = []
+        for classDir in os.listdir(self.validDir) :
+            for imgName in os.listdir(os.path.join(self.validDir,classDir)) :
+                imgPath = os.path.join(self.validDir,classDir,imgName)
+                validFeatureArray.append(self.readImageReturnArray(imgPath))
+                validClassArray.append(classDir)
+        
+        return (np.array(validFeatureArray) , np.array(validClassArray))             
+
+
+if __name__ == "__main__":
+    ROOT_DIR = 'dataFormat'
+    data = PreprocessData(ROOT_DIR)
+    # data.readImageReturnArray("dataFormat/Train/beans/frame1008.jpg")
+  
